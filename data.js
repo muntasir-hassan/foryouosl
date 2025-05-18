349cbeb42f47607ac4b0fd4a390714b8690a9a20
Muntasir Hassan <muntasirhassan17@gmail.com>
2025-05-19 00:41:58 +0600

    Update data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 export const data = [
   {
-    src: "https://youtube.com/shorts/KDNNYUZXDIc",
+    src: "./videos/reel2.mp4",
     title:
       "Back when my awkward old self tried to confess love the only way he knew how... one quiet story, one view, a heart never spoken to 💔. 2023",
   },
