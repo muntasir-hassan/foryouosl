ec19b616905c817adf30709c9c007013965996f2
Muntasir Hassan <muntasirhassan17@gmail.com>
2025-05-20 16:56:46 +0600

    as

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let isShe = false;
-const allowedCountry = process.env.ALLOWED_COUNTRY;
+const allowedCountry = window.ALLOWED_COUNTRY;
 const content = document.getElementById("content");
 import { data } from "./data.js";
 
