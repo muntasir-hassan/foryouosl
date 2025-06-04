f9a1d52b673b6ec624dd8e1bbb3c0069317602f7
Muntasir Hassan <muntasirhassan17@gmail.com>
2025-06-05 01:35:33 +0600

    Update app.js

--- app.js
+++ app.js
@@ -63,7 +63,14 @@ const check = () => {
       <h2 class="text-2xl font-semibold text-gray-700 mb-6">Only for you</h2>
       <div class="grid grid-cols-1 md:grid-cols-2 gap-10">
 
-       
+           <div class="bg-white rounded-lg shadow p-4">
+                  <h3 class="text-lg font-medium mb-3 text-gray-800">
+                    Eid Mubarak 🎀
+                  </h3>
+            <img class="w-full h-auto rounded-md"  loading="lazy" src="eid mubarak.jpg"  />
+
+                  </img></div>
+                  
             ${data
               .map(
                 (reel) => `
